8abda162127c8cf12b7e98e54aa7c70814531e7d
Alan Mestezk <95321834+AlanMestezk@users.noreply.github.com>
2022-06-22 16:24:40 -0300

    Praticando no console

---
+++ OnConsole/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+
+namespace OnConsole {
+    internal class Produto {
+
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque() {
+            return Preco * Quantidade;
+        }
+
+        public override string ToString() {
+            return $"{Nome} " +
+                $"|| Preço: {Preco.ToString("f2", CultureInfo.InvariantCulture)} " +
+                $"|| {Quantidade} unidades"+
+                $"|| Total: {ValorTotalEmEstoque().ToString("f2", CultureInfo.InvariantCulture)} ";
+        }
+    }
+}
---
+++ OnConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+namespace OnConsole {
+    class Program {
+
+        static void Main(string[] args) {
+
+            Produto item = new Produto();   
+            
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.WriteLine(" ");
+
+            Console.Write("Nome: ");
+            item.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            item.Preco = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade: ");
+            item.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Produto: {item}");
+           
+
+        } 
+
+    }
+}
